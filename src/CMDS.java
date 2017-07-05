8602dcc4611bf506a3ae981a39e322c9d681500d
keiki83 <mcallan@uci.edu>
2017-07-04 18:23:09 -0700

    switch to properties file system
    data stored in /servers folder

--- src/CMDS.java
+++ src/CMDS.java
@@ -8,12 +8,6 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.LinkedList;
-
 import javax.swing.JMenuBar;
 import javax.swing.JTabbedPane;
 import javax.swing.SwingUtilities;
@@ -89,38 +83,10 @@ public class CMDS {
 
 	}
 
-	private void loadServers() {
-		LinkedList<String> settingValues = new LinkedList<String>();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("servers.txt"));
-			String line;
-			
-			while((line = br.readLine()) != null) {
-				if(line.length() > 0) {
-					settingValues.add(line);
-				}
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		while(settingValues.size() >= 5) {
-			ServerSettings settings = new ServerSettings();
-			settings.setName(settingValues.pop());
-			settings.setPath(settingValues.pop());
-			settings.setExe(settingValues.pop());
-			settings.setArg(settingValues.pop());
-			if(settingValues.pop().equals("true")) {
-				settings.setRelativePath(true);
-			}
-			else {
-				settings.setRelativePath(false);
-			}
-			tabbedPane.add(settings.getName(), new Server(settings));
+	private void loadServers() {		
+		LoadSettings servers = new LoadSettings("servers/");
+		while(servers.hasSettings()) {
+			tabbedPane.add(servers.peek().getName(), new Server(servers.pop()));
 		}
 	}
 }
---
+++ src/LoadSettings.java
@@ -0,0 +1,79 @@
+/*
+ * Author: Michael R. Callan III
+ * Version: 1.05
+ */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Properties;
+
+public class LoadSettings {
+	private LinkedList<ServerSettings> serverSettings;
+
+	public LoadSettings(String settingsPath) {
+		serverSettings = new LinkedList<ServerSettings>();
+		
+		// Get list of server files
+		File serversFolder = new File(settingsPath);
+		File[] serverFiles = serversFolder.listFiles();
+
+		// Load settings of each file
+		for(int i = 0; i < serverFiles.length; i++) {
+			if(!serverFiles[i].isFile() ||  !serverFiles[i].getName().contains(".properties")) {
+				System.out.println(serverFiles[i].getName());
+				continue;
+			}
+
+			ServerSettings settings = new ServerSettings();
+
+			try {
+				FileInputStream in = new FileInputStream(settingsPath + "/" + serverFiles[i].getName());
+				Properties properties = new Properties();
+				properties.load(in);
+				if(properties.containsKey("name")) {
+					settings.setName(properties.getProperty("name"));
+				}
+				if(properties.containsKey("path")) {
+					settings.setPath(properties.getProperty("path"));
+				}
+				if(properties.containsKey("exe")) {
+					settings.setExe(properties.getProperty("exe"));
+				}
+				if(properties.containsKey("arg")) {
+					settings.setArg(properties.getProperty("arg"));
+				}
+				if(properties.containsKey("relative")) {
+					if(properties.getProperty("relative").equals("true")) {
+						settings.setRelativePath(true);
+					}
+					else {
+						settings.setRelativePath(false);
+					}
+				}			
+				in.close();
+				serverSettings.add(settings);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public Boolean hasSettings() {
+		return !serverSettings.isEmpty();
+	}
+	
+	public ServerSettings pop() {
+		return serverSettings.pop();
+	}
+	
+	public ServerSettings peek() {
+		return serverSettings.peek();
+	}
+}
